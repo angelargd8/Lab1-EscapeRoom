6316d46879d7199f4206a8f2ffd206e43fe941a0
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-02-09 12:04:33 -0600

    feat: add build asset

---
+++ Assets/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMech : MonoBehaviour
+{
+    [SerializeField] AudioClip DoorSFX;
+
+    public Vector3 OpenRotation, CloseRotation;
+
+	public float rotSpeed = 1f;
+
+	public bool doorBool;
+
+
+	void Start()
+	{
+		doorBool = false;
+	}
+
+    void OnTriggerStay(Collider col)
+	{
+		if (col.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+		{
+            AudioManager.Instance.PlaySFX(DoorSFX);
+            gameObject.SetActive(false);
+		}
+	}
+
+	void Update()
+	{
+		if (doorBool)
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(OpenRotation), rotSpeed * Time.deltaTime);
+		else
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(CloseRotation), rotSpeed * Time.deltaTime);
+	}
+
+}
+
